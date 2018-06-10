81329ca96fe1b901497d38c01472a9c4063cce30
Markus Pesch <markus.pesch@cryptic.systems>
2018-06-10 13:39:54 +0200

    add: exercise 12

---
+++ data/uebung_12_-_aufgabe_04.sql
@@ -0,0 +1,7 @@
+CREATE TABEL dwh_regions AS SELECT * FROM dwh.regions;
+CREATE TABEL dwh_countries AS SELECT * FROM dwh.countries;
+CREATE TABEL dwh_customers AS SELECT * FROM dwh.customers;
+CREATE TABEL dwh_orders AS SELECT * FROM dwh.orders;
+CREATE TABEL dwh_order_items AS SELECT * FROM dwh.order_items;
+CREATE TABEL dwh_channels AS SELECT * FROM dwh.channels;
+
--- git-info.sh
+++ git-info.sh
@@ -11,4 +11,9 @@ elif [ $1 == "url" ]; then
   else
     echo $URL
   fi
+elif [ $1 == "contributor" ]; then
+  CONTRIBUTORS=$(git log | grep 'Author:' | cut -d ' ' -f 2,3,4 | sed -e 's/<//g' -e 's/>//g' | sort -u)
+  for line in "$CONTRIBUTORS"; do
+    echo $line
+  done
 fi
