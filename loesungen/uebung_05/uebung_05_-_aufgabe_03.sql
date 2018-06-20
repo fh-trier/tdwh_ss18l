2449470c59fd32b99a4350b5f8d5ff2a572e3f7d
Markus Pesch <markus.pesch@cryptic.systems>
2018-06-20 19:24:17 +0200

    fix: solution exercise 5, task 3

--- loesungen/uebung_05/uebung_05_-_aufgabe_03.sql
+++ loesungen/uebung_05/uebung_05_-_aufgabe_03.sql
@@ -1,25 +1,50 @@
-SELECT
-  dc.region_name AS "REGION",
-  dc.country_name AS "COUNTRY",
-  SUM(f.quantity*f.unit_price) AS "AMOUNT_SOLD",
-  SUM(f.quantity) AS "QUANTITY"
-FROM facts f
-  INNER JOIN dim_customer dc ON (f.cust_id = dc.id)
-  INNER JOIN dim_date dd ON (f.date_id = dd.id)
-WHERE dd.yearnumber = 2012
-GROUP BY dc.region_name, dc.country_name;
+-- Clean
+DROP MATERIALIZED VIEW "REPORT_53";
 
+CREATE MATERIALIZED VIEW "REPORT_53"
+  BUILD IMMEDIATE
+  REFRESH COMPLETE
+  ENABLE QUERY REWRITE
+AS
+  SELECT
+    dd.yearnumber,
+    dd.quarter,
+    CONCAT(de.emp_firstname, CONCAT(', ', de.emp_lastname)) AS "EMPLOYEE",
+    dp.subcategory_name,
+    SUM(f.quantity) AS "ABSATZ"
+  FROM facts f
+    INNER JOIN dim_date dd ON (dd.id = f.date_id)
+    INNER JOIN dim_employee de ON (de.id = f.emp_id)
+    INNER JOIN dim_product dp ON (dp.id = f.prod_id)
+  GROUP BY
+    dd.yearnumber,
+    dd.quarter,
+    de.emp_firstname,
+    de.emp_lastname,
+    dp.subcategory_name;
+
+-- Aktiviere Query-Rewrite für die Session
+ALTER SESSION SET query_rewrite_enabled = true;
+
+-- Greift auf Tabellen anstatt MV zu?
+EXPLAIN PLAN FOR
 SELECT
-  dd.yearnumber AS "YEAR_NUM",
-  dp.category_name AS "CATEGORY",
-  dp.subcategory_name AS "SUB_CATEGORY",
-  SUM(f.quantity)
-FROM facts f
-  INNER JOIN dim_product dp ON (f.prod_id = dp.id)
-  INNER JOIN dim_date dd ON (f.date_id = dd.id)
-  INNER JOIN dim_customer dc ON (f.cust_id = dc.id)
-WHERE Dc.Region_Name in ('Asia','Europe')
-GROUP BY ROLLUP (
-  dp.category_name,
-  dp.subcategory_name
-), dd.yearnumber;
+    dd.yearnumber,
+    dd.quarter,
+    CONCAT(de.emp_firstname, CONCAT(', ', de.emp_lastname)) AS "EMPLOYEE",
+    dp.subcategory_name,
+    SUM(f.quantity) AS "ABSATZ"
+  FROM facts f
+    INNER JOIN dim_date dd ON (dd.id = f.date_id)
+    INNER JOIN dim_employee de ON (de.id = f.emp_id)
+    INNER JOIN dim_product dp ON (dp.id = f.prod_id)
+  GROUP BY
+    dd.yearnumber,
+    dd.quarter,
+    de.emp_firstname,
+    de.emp_lastname,
+    dp.subcategory_name;
+
+-- Übersicht ausgeben lassen
+SELECT *
+FROM TABLE(dbms_xplan.display());
